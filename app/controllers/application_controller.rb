3aa535d3ff8c198a4c7b0283c6f80d9cfc22dc51
Beth Brown <bbrown@cyrusinnovation.com>
2014-06-17 16:34:25 -0400

    Setting up control headers and skip verify auth temporarily

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :set_access_control_headers
+
+  def set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = 'http://127.0.0.1:9000'
+    headers['Access-Control-Request-Method'] = '*'
+  end
 end
--- app/controllers/retrospective_controller.rb
+++ app/controllers/retrospective_controller.rb
@@ -1,4 +1,6 @@
 class RetrospectiveController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+
   def create
     Retrospective.create(:label => params[:label], :start => params[:start])
     render :json => {}
