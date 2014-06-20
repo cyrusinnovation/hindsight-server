7b59ff0450eead45de0ebc91d4b40b019f480ed2
Beth Brown <bbrown@cyrusinnovation.com>
2014-06-20 12:21:35 -0400

    Allow access to the deployed frontend

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   before_filter :set_access_control_headers
 
   def set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = 'http://127.0.0.1:9000'
+    headers['Access-Control-Allow-Origin'] = 'http://127.0.0.1:9000', 'http://cyrus-hindsight.herokuapp.com'
     headers['Access-Control-Request-Method'] = '*'
   end
 end
